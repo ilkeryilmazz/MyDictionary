28eee79ebb118cd99f0c51bdfafd081336520669
ilker yılmaz <ilkergsfb@hotmail.com>
2021-01-21 15:24:48 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        public TValue[] items;
+        public TKey[] keys;
+        public TValue errorArray;
+        //constructor
+        public MyDictionary()
+        {
+            items = new TValue[0];
+            keys = new TKey[0];
+            
+
+    }
+       
+        public void Add(TKey key,TValue item)
+        {
+            
+            TValue[] tempArray = items;
+            TKey[] tempKeys = keys;
+            keys = new TKey[keys.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+            items = new TValue[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+           
+            items[items.Length - 1] = item;
+            
+        }
+        public TValue Yazdir(TKey alias)
+        {
+
+            
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].Equals(alias))
+                {
+                    return items[i];
+                }
+            }
+            return errorArray;
+            
+        }       
+    } 
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+           
+            myDictionary.Add(1, "Masa");
+            myDictionary.Add(2, "Telefon");
+            myDictionary.Add(3, "Bilgisyar");
+            myDictionary.Add(4, "Monitör");
+            Console.WriteLine(myDictionary.Yazdir(1));
+            
+        }
+    }
+}
